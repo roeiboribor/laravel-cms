cdfd7ab4ccf90b0ced108249c96a1352962de4ff
roeiboribor <roeiboribor@gmail.com>
2021-12-22 18:10:57 +0800

    install browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
-    ]);
+    ]).browserSync('laravel-cms.test:8080');
 
 if (mix.inProduction()) {
     mix.version();
