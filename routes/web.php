fffa33521a3cad32f6b80ac9c1707092cdd7a1d3
roeiboribor <roeiboribor@gmail.com>
2021-12-27 17:41:33 +0800

    add navigation-menu routes & navigation dropdown to dashboard layout

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ Route::group(['middleware' => [
     Route::get('/pages', function () {
         return view('admin.pages');
     })->name('pages');
+    Route::get('/navigation-menus', function () {
+        return view('admin.navigation-menus');
+    })->name('navigation-menus');
 });
 
 
